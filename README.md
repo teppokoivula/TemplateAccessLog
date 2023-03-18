378509ae40c147f2044bcda89079dfdf3582e511
Teppo Koivula <teppo.koivula@gmail.com>
2023-03-19 00:23:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Template Access Log for ProcessWire
+-----------------------------------
+
+This ProcessWire module logs changes made to Template role and access settings.
+
+At the moment changes are saved to a log file on disk. Future versions may add more options for logging and displaying logs.
---
+++ TemplateAccessLog.module.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ProcessWire;
+
+class TemplateAccessLog extends WireData implements Module {
+
+	public static function getModuleInfo() {
+		return [
+			'title' => 'Template Access log',
+			'version' => '0.1.0',
+			'summary' => 'Logs changes made to template roles and related access settings',
+			'autoload' => true,
+			'singular' => true,
+			'icon' => 'key',
+		];
+	}
+
+	public function init() {
+		$this->addHookBefore('Templates::save', $this, 'logChanges');
+	}
+
+	protected function logChanges(HookEvent $event) {
+
+		$item = $event->arguments(0);
+
+		$data = $this->database
+			->query('SELECT data FROM templates WHERE id = ' . (int) $item->id)
+			->fetchColumn();
+		$data = json_decode($data, true);
+
+		$permissions = [
+			'view' => [
+				'prop' => 'roles',
+				'data' => $item->roles->explode('id'),
+			],
+			'edit' => [
+				'prop' => 'editRoles',
+				'data' => $item->editRoles,
+			],
+			'add' => [
+				'prop' => 'addRoles',
+				'data' => $item->addRoles,
+			],
+			'create' => [
+				'prop' => 'createRoles',
+				'data' => $item->createRoles,
+			],
+		];
+
+		$permissions_changed = $item->id < 1 ? array_filter(array_map(function ($permission) {
+			return array_filter($permission['data']);
+		}, $permissions)) : [];
+		if (empty($permissions_changed)) {
+			foreach ($permissions as $permission => $permission_data) {
+				if (($data[$permission_data['prop']] ?? []) != $permission_data['data']) {
+					$permissions_changed[$permission] = $this->getArrayDiff(
+						$data[$permission_data['prop']] ?? [],
+						$permission_data['data']
+					);
+				}
+			}
+		}
+
+		if ($item->isChanged('useRoles') || !empty($permissions_changed)) {
+			$this->wire->log->save(
+				'template_access_log',
+				json_encode([
+					'template' => $item->name,
+					'template_id' => $item->id,
+					'use_roles' => $item->useRoles,
+					'permissions' => array_map(function ($permission) {
+						return $permission['data'];
+					}, $permissions),
+					'permissions_changed' => $permissions_changed,
+				])
+			);
+		}
+	}
+
+	protected function getArrayDiff($old_array, $new_array): array {
+		return array_merge(array_diff($new_array, $old_array), array_map(function($removal) {
+			return -$removal;
+		}, array_diff($old_array, $new_array)));
+	}
+}
